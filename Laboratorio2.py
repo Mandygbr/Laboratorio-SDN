2e66731e319e402ac141b9bb0ff3a29147ccfb50
Mandygbr <95167433+Mandygbr@users.noreply.github.com>
2021-12-16 23:23:59 -0400

    guarda la lista de los dispositivos appliance y wireless

--- Laboratorio2.py
+++ Laboratorio2.py
@@ -1,11 +1,14 @@
 import requests
 import pprint
 import json
+import csv
 
 url = "https://api.meraki.com/api/v1/organizations"
 
 payload = None
-i=0
+i = 0
+k = 0
+j = 0
 organizaciones=[]
 headers = {
     "Content-Type": "application/json",
@@ -15,9 +18,32 @@ headers = {
 
 lista = requests.request('GET', url, headers=headers, data = payload)
 lista = lista.json()
+pprint.pprint(lista)
+
 while i < len(lista):
     organizaciones.append(lista[i]['name'])
     print(organizaciones[i])
     i=i+1
 
 
+url2 = "https://api.meraki.com/api/v1/organizations/681155/devices?productTypes%5B%5D=wireless"
+url3 = "https://api.meraki.com/api/v1/organizations/681155/devices?productTypes%5B%5D=appliance"
+disp_wireless=requests.get(url2, headers=headers, data = payload)
+disp_appliance=requests.get(url3, headers=headers, data = payload)
+dev_wir=disp_wireless.json()
+dev_appli=disp_appliance.json()
+pprint.pprint(dev_wir)
+pprint.pprint(dev_appli)
+
+
+lista_dispositivos = open('devices.csv', 'w')
+with lista_dispositivos:
+    writer = csv.writer(lista_dispositivos)
+    while k < len(dev_wir):
+        if (dev_wir[k]['productType'] == "wireless"):
+            writer.writerows(dev_wir[k])
+        k = k+1
+    while j < len(dev_appli):
+         if (dev_appli[j]['productType'] == "apppliance"):
+            writer.writerows(dev_appli[j])
+         j = j+1
